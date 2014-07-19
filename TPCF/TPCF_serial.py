d54a2f4ffbac18b3f4f3ed96fe81e98259926376
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-07-19 16:59:03 -0600

    cleaned up TPCF_serial and removed internal test

--- TPCF/TPCF_serial.py
+++ TPCF/TPCF_serial.py
@@ -5,81 +5,29 @@
 #June 17, 2014
 #calculate the 2-point correlation function serially
 
-#for cross-correlation call as
-#    python serial_TPCF.py output.dat input1.dat input2.dat
-#for test
-#    python serial_TPCF.py
-
-import sys
 
 def main():
-	#calculate correlation function given data
-    import numpy as np
-
-    if len(sys.argv)==1:
-        print 'running test of pair counter...'
-        test()
-    else: 
-        print 'running cross-correlation...'
-        savename = sys.argv[1]
-        filename_1 = sys.argv[2]
-        filename_2 = sys.argv[3]
-        #read in data
-        from astropy.io import ascii
-        from astropy.table import Table
-        data_1 = ascii.read(filename_1)
-        data_2 = ascii.read(filename_2) 
-        
-        #convert tables into numpy arrays
-        data_1 = np.array([data_1[c] for c in data_1.columns]).T
-        data_2 = np.array([data_2[c] for c in data_2.columns]).T
-        
-    	#define log radial bins
-    	bins = np.arange(0,1,0.01)
-    	bins = np.arange(-3,0.5,0.1)
-    	bins = 10.0**bins
-    	bin_centers = (bins[:-1]+bins[1:])/2.0
-    
-    	#count pairs
-    	DD,RR,DR,bins = npairs(data_1, data_2, bins)
-    	factor = (len(data_2)*1.0)/len(data_1)
-    	
-    	print DR
-    
-    	#use estimator of 2PCF
-    	corr = TPCF_estimator(DD,RR,DR,factor)
-    
-    	data = Table([bins[1:], corr], names=['r', 'corr'])
-        ascii.write(data, savename)
-        
-        #print data
-    
-    
-def test(N_points_1=100):
-    #tests pair counter with a random sample
-    import numpy as np
-    import matplotlib.pyplot as plt
-	
     '''
-    #Create a random 2-D distribution
-    N_points_2 = 20 * N_points_1 #random sample
-    data_1 = np.random.random((N_points_1,2)) #random points in range {[0,1),[0,1)}
-    data_2 = np.random.random((N_points_2,2)) #random points in range {[0,1),[0,1)}
+    example:
+    python TPCF_serial.py output.dat input1.dat input2.dat
     '''
+    import sys
+    import numpy as np
     
-    #Create a random 3-D distribution
-    N_points_2 = 20 * N_points_1 #random sample
-    data_1 = np.random.random((N_points_1,3)) #random points in range {[0,1),[0,1),[0,1)}
-    data_2 = np.random.random((N_points_2,3)) #random points in range {[0,1),[0,1),[0,1)}
-    
-    #save test data
-    from astropy.table import Table
+    print "running cross-correlation..."
+    savename = sys.argv[1]
+    filename_1 = sys.argv[2]
+    filename_2 = sys.argv[3]
+    #read in data
     from astropy.io import ascii
-    table_1 = Table([data_1[:,0],data_1[:,1],data_1[:,2]], names=['x', 'y', 'z'])
-    ascii.write(table_1, 'test_D.dat')
-    table_2 = Table([data_2[:,0],data_2[:,1],data_2[:,2]], names=['x', 'y', 'z'])
-    ascii.write(table_2, 'test_R.dat')
-    
+    from astropy.table import Table
+    data_1 = ascii.read(filename_1)
+    data_2 = ascii.read(filename_2) 
+        
+    #convert tables into numpy arrays
+    data_1 = np.array([data_1[c] for c in data_1.columns]).T
+    data_2 = np.array([data_2[c] for c in data_2.columns]).T
+        
     #define log radial bins
     bins = np.arange(0,1,0.01)
     bins = np.arange(-3,0.5,0.1)
@@ -90,22 +38,40 @@ def test(N_points_1=100):
     DD,RR,DR,bins = npairs(data_1, data_2, bins)
     factor = (len(data_2)*1.0)/len(data_1)
     
+    print DR
+    
     #use estimator of 2PCF
     corr = TPCF_estimator(DD,RR,DR,factor)
     
-    #plot result
-    plt.figure()
-    plt.plot(bin_centers, corr+1, '.-', color='black')
-    plt.xscale('log')
-    plt.xlim([0.001,2])
-    plt.ylim([0,2])
-    plt.show()
+    data = Table([bins[1:], corr], names=['r', 'corr'])
+    ascii.write(data, savename)
+    print "done."
+
+
+def TPCF(data_1, data_2, bins):
+    '''
+    Calculates the two point correlation function.
+    paramaters
+    data_1: N,k array or list of points
+    data_2: N,k array or list of points
+    returns
+    correlation function, bins
+    '''
     
+    #count pairs
+    DD,RR,DR,bins = npairs(data_1, data_2, bins)
+    factor = (len(data_2)*1.0)/len(data_1)
+    
+    #use estimator of 2PCF
+    corr = TPCF_estimator(DD,RR,DR,factor)
+    
+    return corr, bins
+
 
 def TPCF_estimator(DD,RR,DR,factor):
     #estimate correlation function
     corr = (factor ** 2.0 * DD - 2.0 * factor * DR + RR) / RR
-
+    
     return corr
     
     
@@ -117,7 +83,7 @@ def npairs(data_1, data_2, bins):
     data_1 = np.asarray(data_1)
     data_2 = np.asarray(data_2)
     bins   = np.asarray(bins)
-
+    
     KDT_1 = cKDTree(data_1)
     KDT_2 = cKDTree(data_2)
     
@@ -127,7 +93,7 @@ def npairs(data_1, data_2, bins):
     DD_11     = np.diff(counts_11)
     DD_22     = np.diff(counts_22)
     DD_12     = np.diff(counts_12)
-	
+    
     return DD_11, DD_22, DD_12, bins
 
 
