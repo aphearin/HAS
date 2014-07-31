a2aea63d9123779d68683d685ea3fa6aff0b54b7
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-07-31 15:57:26 -0400

    updated API to allow for call as a module function

--- TPCF/npairs_mpi.py
+++ TPCF/npairs_mpi.py
@@ -5,9 +5,11 @@
 #July 19, 2014
 #calculate all pairs and store the numbers in bins
 
+from __future__ import print_function
 from mpi4py import MPI
 import numpy as np 
 import sys
+import os
 import math
 import time
 from scipy.spatial import cKDTree
@@ -17,16 +19,27 @@ def main():
     example:
     mpirun -np 4 python npairs_mpi.py output.dat input1.dat input2.dat
     '''
-    
     comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+    rank = comm.rank
     
-    print 'running rank:', rank
+    #this first option is for my test files, so only works if you have a copy or make one!
+    if len(sys.argv)==1:
+        savename = './test/test_data/test_out.dat'
+        filename_1 = './test/test_data/test_D.dat'
+        filename_2 = './test/test_data/test_R.dat'
+        if (not os.path.isfile(filename_1)) | (not os.path.isfile(filename_2)):
+            raise ValueError('Please provide vailid filenames for data input.')
+        else:
+            if rank==0:
+                print('Running code with default test data files. Saving output as:', savename)
+    elif len(sys.argv)==4:
+        savename = sys.argv[1]
+        filename_1 = sys.argv[2]
+        filename_2 = sys.argv[3]
+        print('Running code with user supplied data files. Saving output as:', savename)
+    else:
+        raise ValueError('Please provide a fielpath to save output and two data files to read.')
     
-    #read in data
-    filename_1 = sys.argv[2]
-    filename_2 = sys.argv[3]
     from astropy.io import ascii
     from astropy.table import Table
     data_1 = ascii.read(filename_1)
@@ -36,18 +49,38 @@ def main():
     data_1 = np.array([data_1[c] for c in data_1.columns]).T
     data_2 = np.array([data_2[c] for c in data_2.columns]).T
     
-    #how many points are in each array
-    N1 = len(data_1)
-    N2 = len(data_2)
-    if comm.rank==0:
-        print N1, 'x', N2, 'points'
-    
     #define radial bins.  This should be made an input at some point.
-    bins = np.arange(0,1,0.01)
-    bins = np.arange(-4,1,0.1)
-    bins = 10.0**bins
+    #bins = np.arange(0,1,0.01)
+    #bins = np.arange(-4,1,0.1)
+    bins=np.logspace(-1,1.5,10)
     bin_centers = (bins[:-1]+bins[1:])/2.0
     
+    DD,RR,DR,bins = npairs(data_1, data_2, bins, period=None, comm=comm)
+    
+def npairs(data_1, data_2, bins, period=None, comm=None):
+    '''
+    Count pairs with separations given by bins.
+    parameters
+        data_1: N,k array or list of points
+        data_2: N,k array or list of points
+        bins: array or list of continuos bins edges
+        period: length k array defining axis aligned PBCs. If set to none, PBCs = infinity.
+        comm: mpi Intracommunicator object, or None (run on 1 core)
+    returns:
+        DD_11: data_1-data_1 pairs (auto correlation)
+        DD_22: data_1-data_2 pairs (auto correlation)
+        DD_12: data_2-data_2 pairs (cross-correlation)
+        bins
+    '''
+    if comm==None: 
+        rank = 0
+        size = 1
+    else: 
+        rank = comm.rank
+        size = comm.Get_size()
+    
+    N1 = len(data_1)
+    N2 = len(data_2)
     N3 = len(bins)
     
     #define the indices
@@ -57,17 +90,19 @@ def main():
     #split up indices for each subprocess
     sendbuf_1=[] #need these as place holders till each process get its list
     sendbuf_2=[]
-    if comm.rank==0:
+    if rank==0:
         chunks = np.array_split(inds1,size)
         sendbuf_1 = chunks
         chunks = np.array_split(inds2,size)
         sendbuf_2 = chunks
     
-    #send out lists of indices for each subprocess to use
-    inds1=comm.scatter(sendbuf_1,root=0)
-    inds2=comm.scatter(sendbuf_2,root=0)
+    if comm!=None:
+        #send out lists of indices for each subprocess to use
+        inds1=comm.scatter(sendbuf_1,root=0)
+        inds2=comm.scatter(sendbuf_2,root=0)
     
-    #create trees
+    #creating trees seems very cheap, so I don't worry about this too much.
+    #create full trees
     KDT_1 = cKDTree(data_1)
     KDT_2 = cKDTree(data_2)
     #create chunked up trees
@@ -82,19 +117,31 @@ def main():
     DD_22     = np.diff(counts_22)
     DD_12     = np.diff(counts_12)
     
-    #gather results from each subprocess
-    DD_11 = comm.gather(DD_11,root=0)
-    DD_22 = comm.gather(DD_22,root=0)
-    DD_12 = comm.gather(DD_12,root=0)
-    
-    if comm.rank==0:
+    if comm==None:
+        return DD_11, DD_22, DD_12, bins
+    else:
+        #gather results from each subprocess
+        DD_11 = comm.gather(DD_11,root=0)
+        DD_22 = comm.gather(DD_22,root=0)
+        DD_12 = comm.gather(DD_12,root=0)
+        
+    if (rank==0) & (comm!=None):
         #combine counts from subprocesses
         DD_11=np.sum(DD_11, axis=0)
         DD_22=np.sum(DD_22, axis=0)
         DD_12=np.sum(DD_12, axis=0)
-        savename= sys.argv[1]
-        data = Table([bins[1:], DD_11, DD_22, DD_12], names=['r', 'DD_11','DD_22','DD_12'])
-        ascii.write(data, savename)
+        #send result out to other processes
+        DD_11 = comm.bcast(DD_11, root=0)
+        DD_22 = comm.bcast(DD_22, root=0)
+        DD_12 = comm.bcast(DD_12, root=0)
+    
+    #receive result from rank 0
+    DD_11 = comm.bcast(DD_11, root=0)
+    DD_22 = comm.bcast(DD_22, root=0)
+    DD_12 = comm.bcast(DD_12, root=0)
+    
+    return DD_11, DD_22, DD_12, bins
+
 
 if __name__ == '__main__':
     main()
